639a078b559940fb0976c4d5404d27ae5a38a097
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-12-21 15:52:15 +0100

    commit

--- src/components/MyDetail.jsx
+++ src/components/MyDetail.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import CardComp from "./CardComp";
 import { Col, Row } from "react-bootstrap";
+import AddCommentComp from "./AddCommentComp";
+import CommentAreaComp from "./CommentAreaComp";
 
 function MyDetail(props) {
   return (
-    <Row className=" justify-content-center">
+    <Row className=" justify-content-center min-vh-100">
       <Col xs={4}>
         <CardComp book={props.bookObj} />
+      </Col>
+      <Col xs={12}>
+        <AddCommentComp />
+        <CommentAreaComp />
       </Col>
     </Row>
   );
